9441b728f6dd47ee8fca86fb016906dd30fb07bf
shubhamV02 <shubhamvhanale.arag@gmail.com>
2025-07-04 20:28:05 +0530

    Fourth Commit

--- git/src/gitproject/Demofirst.java
+++ git/src/gitproject/Demofirst.java
@@ -4,13 +4,13 @@ public class Demofirst {
   
   
   public static void main(String[] args) {
-	  public int Substraction() {
+	   
 		  
 		  int a=400;
-		  int b=200;s
+		  int b=200;
 		  
 		  System.out.println("Result: "+a+b);  
-	  }
+	  
   }
   
 }
